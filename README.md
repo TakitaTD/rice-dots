0457054512dd37aed23aa1fbd0f05e967040458c
Tim Takita <takitatd@gmail.com>
2022-09-09 16:36:10 +1000

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+## README
+
+The configs in polybar are tweaked to my machine, (network adapters, batteries) please replace them to fit your device.
+
+# Install dependencies (Arch)
+
+```
+paru -S --needed nerd-fonts-complete
+```
---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "Making Backups..."
+mkdir -p $HOME/.config/backups $HOME/.local/share/fonts
+cp -r $HOME/.config/i3 $HOME/.config/backups
+cp -r $HOME/.config/polybar $HOME/.config/backups
+cp -r $HOME/.config/rofi $HOME/.config/backups
+cp -r $HOME/.config/picom.conf $HOME/.config/backups
+echo "Installing..."
+cp -r i3 $HOME/.config
+cp -r polybar $HOME/.config
+cp -r rofi $HOME/.config
+cp -r picom.conf $HOME/.config
+echo "Installing fonts..."
+cp -r fonts/** $HOME/.local/share/fonts
